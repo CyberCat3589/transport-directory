6bf5d70a9ce5d9f4cc1a830aca641fbf2152a043
CyberCat <Vgnetnet156@gmail.com>
2024-04-18 08:42:46 +0300

    Создан класс справочника с основными полями

--- transport_catalogue.h
+++ transport_catalogue.h
@@ -1,6 +1,29 @@
 #pragma once
+#include <deque>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include "geo.h"
+
+struct Stop
+{
+    std::string name;
+    Coordinates coordinates;
+};
+
+struct Bus
+{
+    std::string name;
+    std::vector<Stop*> stops;
+};
 
 class TransportCatalogue
 {
-	// Реализуйте класс самостоятельно
+  public:
+  private:
+    std::deque<Stop> stops_;
+    std::deque<Bus> buses_;
+    std::unordered_map<std::string_view, Stop*> stopname_to_stop_;
+    std::unordered_map<std::string_view, Bus*> busname_to_bus;
 };
